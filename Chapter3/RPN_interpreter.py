eb2fb122a78483192ae68aa291180f0140411fd8
HPC2H2 <hpc2h2@outlook.com>
2025-03-12 21:20:48 +0800

    优化 RPN 解释器，增强错误处理并更新输入提示

--- Chapter3/RPN_interpreter.py
+++ Chapter3/RPN_interpreter.py
@@ -5,32 +5,35 @@ Description: RPN interpreter
 """
 
 def main():
-    str_exp = input("Enter an RPN expression: ")
+    str_exp = input("Enter an RPN expression(only for intergers and +-*/): ")
     list_exp = str_exp.split()
     stack = []
-    for e in list_exp:
-        if e.isdigit():
-            stack.append(int(e))
-        else:
+    for item in list_exp:
+        if item.isdigit():
+            stack.append(int(item))
+        elif item in "+-*/":
             try:
                 b = stack.pop()
                 a = stack.pop()
-                if e == "+":
-                    stack.append(a + b)
-                elif e == "-":
-                    stack.append(a - b)
-                elif e == "*":
-                    stack.append(a * b)
-                elif e == "/":
-                    stack.append(a / b)
-                else:
-                    print("Invalid operator")
             except IndexError:
-                print("Invalid expression")
-            finally:
-                if len(stack) > 1:
-                    print("多余的的数字操作数")
-                print("The result is", stack.pop())
+                raise IndexError("Too many operands, or too few operators")
+            except ZeroDivisionError:
+                pass
+
+            if item == '+':
+                stack.append(a + b)
+            elif item == '-':
+                stack.append(a - b)
+            elif item == '*':
+                stack.append(a * b)
+            elif item == '/':
+                stack.append(a / b)
+        else:
+            raise ValueError("Invalid operator")
+    
+    if len(stack) > 1:
+        raise IndexError("Too many operands, or too few operators")
+    print("The result is", stack.pop())
     
 
 if __name__ == "__main__":
